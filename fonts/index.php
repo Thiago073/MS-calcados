4b372c098402d2f68174ddb3741a8c788fab1b32
thiago santos <thiagodetarso94@gmail.com>
2021-07-16 21:47:15 -0300

    Inicialização do index html.

---
+++ fonts/index.php
@@ -0,0 +1,26 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="language" content="pt-BR">
+    <link rel="shortcut icon" sizes="16x16" href="/img/logo.jpg">  
+    <title>MS Calçados</title>
+</head>
+<body>
+    <div class="menu">
+        <ul>
+            <li><a href="index.html">MS Calçados</a></li>
+            <li><a href="conhecimentos.html">Sapatos</a></li>
+            <li><a href="projetos.html">jnjsdbjs</a></li>
+            <li><a href="contato.html">Contato</a></li>
+            <li><a href="login.html">Cadastre-se</a></li>			
+        </ul>
+    </div>
+   
+</body>
+</html>
--- tabelas/endereco.sql
+++ tabelas/endereco.sql
@@ -1,5 +1,5 @@
 CREATE TABLE endereco(
-    endr_sequencial SERIAL PRIMARY,
+    endr_sequencial SERIAL PRIMARY KEY,
     endr_rua VARCHAR(255),
     endr_complemento VARCHAR(255),
     endr_cep CHAR(8)
